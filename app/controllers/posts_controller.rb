ab507af13949c8179066421f9c1ccbccdda42b9d
brianforhan <brian.forhan@kellogg.northwestern.edu>
2022-02-21 17:52:11 +0000

    commit #6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,14 @@ class PostsController < ApplicationController
 
     def new
         @post = Post.new
+        @place = Place.find(params["id"])
+        @post.id = @place.id
     end
     
     def create
         @post = Post.new(params["post"])
         @post.save
-        redirect_to "/places/#{@post.place.id}"
+        redirect_to "/places"
     end
 
 end
